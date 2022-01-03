a63b87a80ce83a8bc262a4f75e966786116dfad7
afaf-tech <fatihalfikri5@gmail.com>
2022-01-04 00:11:25 +0800

    init project research c# 10 and dotnet 6

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using SampleNamespace.NestedNamespace;
+class AnotherSampleClass
+{
+    static public void Main(String[] args){
+        new SampleClass2();
+        Console.WriteLine("Main Method");
+    }
+}
